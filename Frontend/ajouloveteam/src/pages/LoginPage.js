b78aa9179ad8c0104e5fe93607aa3969688422f7
MyeongJunSong <song2948837@gmail.com>
2023-04-16 22:15:54 +0900

    아이디 찾기 페이지 UI 구현

--- Frontend/ajouloveteam/src/pages/LoginPage.js
+++ Frontend/ajouloveteam/src/pages/LoginPage.js
@@ -1,17 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 function LoginPage() {
   const [id, setId] = useState("");
   const [pwd, setPwd] = useState("");
+  const [email, setEmail] = useState("");
+  const [open, setOpen] = React.useState(false);
   const changeId = (event) => {
     setId(event.target.value);
   };
   const changePwd = (event) => {
     setPwd(event.target.value);
   };
+  const changeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <div className="auth-wrapper">
       <div style={{ textAlign: "center" }}>
@@ -30,9 +48,50 @@ function LoginPage() {
         <button className="button" type="button">
           제출
         </button>
-        <Link style={{ color: "gray", textDecoration: "none" }} to="/register">
-          아이디가 없다면...{" "}
-        </Link>
+        <div className="loginPage-button">
+          <Link
+            style={{ color: "gray", textDecoration: "none" }}
+            to="/register"
+          >
+            아이디가 없다면...{" "}
+          </Link>
+          <button
+            type="button"
+            onClick={handleClickOpen}
+            style={{
+              color: "gray",
+              textDecoration: "none",
+              background: "white",
+              border: "none",
+              fontSize: "16px",
+            }}
+          >
+            아이디 찾기
+          </button>
+        </div>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>아이디 찾기</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              아이디 찾기를 위해 이메일을 입력하세요.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              value={email}
+              onChange={changeEmail}
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>확인</Button>
+            <Button onClick={handleClose}>닫기</Button>
+          </DialogActions>
+        </Dialog>
       </form>
     </div>
   );
