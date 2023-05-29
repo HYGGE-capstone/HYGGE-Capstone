36ebf7e4c775f968cb5f5e8b096d53f8160f9cb8
MyeongJunSong <song2948837@gmail.com>
2023-05-29 21:45:00 +0900

    관리자 페이지 커밋

--- Frontend/ajouloveteam/src/admin/AdminPage.js
+++ Frontend/ajouloveteam/src/admin/AdminPage.js
@@ -18,18 +18,16 @@ function AdminPage() {
   const [open2, setOpen2] = React.useState(false);
   const [open3, setOpen3] = React.useState(false);
   const [open4, setOpen4] = React.useState(false);
+  const [open5, setOpen5] = React.useState(false);
   const [schoolList, setSchoolList] = useState([]);
-  const [subjectList, setSubjectList] = useState([
-    { id: 1, name: "캡스톤디자인" },
-    { id: 2, name: "캡스톤디자인2" },
-    { id: 3, name: "캡스톤디자인3" },
-  ]);
+  const [subjectList, setSubjectList] = useState([]);
   const [teamList, setTeamList] = useState([
     { id: 1, name: "HYGGE" },
     { id: 2, name: "HYGGE2" },
     { id: 3, name: "HYGGE3" },
   ]);
-  const [selectSchool, setSelectSchool] = useState("");
+  const [selectSchoolId, setSelectSchoolId] = useState();
+  const [selectSubjectId, setSelectSubjectId] = useState();
   const [adminFlag, setAdminFlag] = useState(0);
   const [userId, setUserId] = useRecoilState(useridState);
   const [schoolName, setSchoolName] = useState("");
@@ -37,9 +35,11 @@ function AdminPage() {
   const [schoolId, setSchoolId] = useState();
   const [SubjectName, setSubjectName] = useState("");
   const [SubjectCode, setSubjectCode] = useState("");
+  const [SubjectYear, setSubjectYear] = useState();
   const [SubjectSemester, setSubjectSemester] = useState("");
   const [SubjectTime, setSubjectTime] = useState("");
   const [SubjectPName, setSubjectPName] = useState("");
+  const [teamInfo, setTeamInfo] = useState([]);
   const navigate = useNavigate();
   const navigateToAdmin = () => {
     window.location.reload();
@@ -75,13 +75,44 @@ function AdminPage() {
 
   const handleClose3 = () => {
     setOpen3(false);
+    setSubjectName("");
+    setSubjectCode("");
+    setSubjectYear();
+    setSubjectSemester("");
+    setSubjectTime("");
+    setSubjectPName("");
   };
-  const handleClickOpen4 = () => {
+  const handleClickOpen4 = (data) => {
+    setSelectSubjectId(data.subjectId);
+    setSchoolId(data.schoolId);
+    setSubjectName(data.name);
+    setSubjectCode(data.code);
+    setSubjectYear(data.year);
+    setSubjectSemester(data.semester);
+    setSubjectTime(data.time);
+    setSubjectPName(data.professorName);
     setOpen4(true);
   };
 
   const handleClose4 = () => {
     setOpen4(false);
+    setSubjectName("");
+    setSubjectCode("");
+    setSubjectYear();
+    setSubjectSemester("");
+    setSubjectTime("");
+    setSubjectPName("");
+  };
+
+  const handleClickOpen5 = (data) => {
+    setTeamInfo(Object.values(data));
+    console.log(Object.values(data));
+    setOpen5(true);
+  };
+
+  const handleClose5 = () => {
+    setTeamInfo([]);
+    setOpen5(false);
   };
   const changeSchoolName = (event) => {
     setSchoolName(event.target.value);
@@ -95,6 +126,9 @@ function AdminPage() {
   const changeSubjectCode = (event) => {
     setSubjectCode(event.target.value);
   };
+  const changeSubjectYear = (event) => {
+    setSubjectYear(event.target.value);
+  };
   const changeSubjectSemester = (event) => {
     setSubjectSemester(event.target.value);
   };
@@ -103,6 +137,17 @@ function AdminPage() {
   };
   const changeSubjectPName = (event) => {
     setSubjectPName(event.target.value);
+  };
+  const schoolManage = (schoolId) => {
+    setSelectSchoolId(schoolId);
+    setAdminFlag(1);
+    getSubjectList(schoolId);
+  };
+
+  const subjectManage = (subjectId) => {
+    setSelectSubjectId(subjectId);
+    setAdminFlag(2);
+    getTeamList(subjectId);
   };
   const addSchool = async () => {
     const school = {
@@ -131,9 +176,103 @@ function AdminPage() {
       .then((resp) => {
         console.log(resp);
         handleClose2();
+        getSchoolList();
       })
       .catch((err) => {
         handleClose2();
+        alert(err.response.data.message);
+      });
+  };
+  const addSubject = async () => {
+    const subject = {
+      schoolId: selectSchoolId,
+      name: SubjectName,
+      code: SubjectCode,
+      year: SubjectYear,
+      semester: SubjectSemester,
+      time: SubjectTime,
+      professorName: SubjectPName,
+    };
+    await api
+      .post(`admin/subject`, subject)
+      .then((resp) => {
+        console.log(resp);
+        handleClose3();
+        getSubjectList(selectSchoolId);
+      })
+      .catch((err) => {
+        handleClose3();
+        alert(err.response.data.message);
+      });
+  };
+
+  const subjectChange = async () => {
+    console.log(selectSubjectId);
+    const subject = {
+      subjectId: selectSubjectId,
+      schoolId: selectSchoolId,
+      name: SubjectName,
+      code: SubjectCode,
+      year: SubjectYear,
+      semester: SubjectSemester,
+      time: SubjectTime,
+      professorName: SubjectPName,
+    };
+    await api
+      .put(`admin/subject`, subject)
+      .then((resp) => {
+        console.log(resp);
+        handleClose4();
+        getSubjectList(selectSchoolId);
+      })
+      .catch((err) => {
+        handleClose4();
+        alert(err.response.data.message);
+      });
+  };
+
+  const subjectDelete = async (subjectId) => {
+    await api
+      .delete(`admin/subject?subjectId=${subjectId}`)
+      .then((resp) => {
+        console.log(resp);
+        getSubjectList(selectSchoolId);
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
+      });
+  };
+
+  const teamDelete = async (teamId) => {
+    await api
+      .delete(`admin/team?teamId=${teamId}`)
+      .then((resp) => {
+        console.log(resp);
+        getTeamList(selectSubjectId);
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
+      });
+  };
+  const getTeamList = async (subjectId) => {
+    await api
+      .get(`admin/team?subjectId=${subjectId}`)
+      .then((resp) => {
+        console.log(resp);
+        setTeamList(resp.data.teams);
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
+      });
+  };
+  const getSubjectList = async (schoolId) => {
+    await api
+      .get(`admin/subject?schoolId=${schoolId}`)
+      .then((resp) => {
+        console.log(resp);
+        setSubjectList(resp.data);
+      })
+      .catch((err) => {
         alert(err.response.data.message);
       });
   };
@@ -198,6 +337,7 @@ function AdminPage() {
                     width: "100px",
                     fontSize: "10px",
                   }}
+                  onClick={() => handleClickOpen3()}
                 >
                   수업 추가
                 </Button>
@@ -232,7 +372,7 @@ function AdminPage() {
                           <Dropdown.Menu>
                             <Dropdown.Item
                               href="#/action-1"
-                              onClick={() => setAdminFlag(1)}
+                              onClick={() => schoolManage(data.schoolId)}
                             >
                               학교 관리
                             </Dropdown.Item>
@@ -248,9 +388,6 @@ function AdminPage() {
                             >
                               학교 수정
                             </Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">
-                              학교 삭제
-                            </Dropdown.Item>
                           </Dropdown.Menu>
                         </Dropdown>
                       </div>
@@ -260,7 +397,9 @@ function AdminPage() {
                   subjectList.map((data, index) => {
                     return (
                       <div className="admin-content" key={index}>
-                        <div className="admin-content-name">{data.name}</div>
+                        <div className="admin-content-name">
+                          {data.subjectId} {data.name}({data.code})
+                        </div>
                         <Dropdown as={ButtonGroup}>
                           <Dropdown.Toggle
                             split
@@ -279,14 +418,20 @@ function AdminPage() {
                           <Dropdown.Menu>
                             <Dropdown.Item
                               href="#/action-1"
-                              onClick={() => setAdminFlag(2)}
+                              onClick={() => subjectManage(data.subjectId)}
                             >
                               수업 관리
                             </Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">
+                            <Dropdown.Item
+                              href="#/action-2"
+                              onClick={() => handleClickOpen4(data)}
+                            >
                               수업 수정
                             </Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">
+                            <Dropdown.Item
+                              href="#/action-3"
+                              onClick={() => subjectDelete(data.subjectId)}
+                            >
                               수업 삭제
                             </Dropdown.Item>
                           </Dropdown.Menu>
@@ -298,7 +443,9 @@ function AdminPage() {
                   teamList.map((data, index) => {
                     return (
                       <div className="admin-content" key={index}>
-                        <div className="admin-content-name">{data.name}</div>
+                        <div className="admin-content-name">
+                          {data.teamName}
+                        </div>
                         <Dropdown as={ButtonGroup}>
                           <Dropdown.Toggle
                             split
@@ -315,10 +462,16 @@ function AdminPage() {
                           />
 
                           <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-2">
-                              팀 수정
+                            <Dropdown.Item
+                              href="#/action-1"
+                              onClick={() => handleClickOpen5(data)}
+                            >
+                              팀 정보보기
                             </Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">
+                            <Dropdown.Item
+                              href="#/action-3"
+                              onClick={() => teamDelete(data.teamId)}
+                            >
                               팀 삭제
                             </Dropdown.Item>
                           </Dropdown.Menu>
@@ -450,7 +603,7 @@ function AdminPage() {
         PaperProps={{ sx: { width: "60%", height: "60%" } }}
       >
         <DialogTitle style={{ background: "#072e5d", color: "white" }}>
-          팀 생성
+          과목 추가
         </DialogTitle>
         <DialogContent
           style={{
@@ -461,10 +614,12 @@ function AdminPage() {
           }}
         >
           <div className="resume-dialog-wrapper" style={{ height: "80%" }}>
-            팀 이름
+            과목 이름
             <TextField
               id="outlined-textarea"
               variant="standard"
+              value={SubjectName}
+              onChange={changeSubjectName}
               style={{
                 width: "100%",
                 marginBottom: "12px",
@@ -472,12 +627,13 @@ function AdminPage() {
               InputProps={{
                 style: {},
               }}
-              multiline
             />
-            팀 주제
+            과목 코드
             <TextField
               id="outlined-textarea"
               variant="standard"
+              value={SubjectCode}
+              onChange={changeSubjectCode}
               style={{
                 width: "100%",
                 marginBottom: "12px",
@@ -485,25 +641,219 @@ function AdminPage() {
               InputProps={{
                 style: {},
               }}
-              multiline
             />
-            팀 설명
-            <input
-              type="number"
-              min="2"
+            과목 년도
+            <TextField
+              id="outlined-textarea"
+              variant="standard"
+              value={SubjectYear}
+              onChange={changeSubjectYear}
               style={{
-                marginTop: "12px",
-                width: "20%",
-                fontSize: "15px",
+                width: "100%",
+                marginBottom: "12px",
               }}
-            ></input>
+              InputProps={{
+                style: {},
+              }}
+            />
+            과목 학기
+            <TextField
+              id="outlined-textarea"
+              variant="standard"
+              value={SubjectSemester}
+              onChange={changeSubjectSemester}
+              style={{
+                width: "100%",
+                marginBottom: "12px",
+              }}
+              InputProps={{
+                style: {},
+              }}
+            />
+            과목 시간
+            <TextField
+              id="outlined-textarea"
+              variant="standard"
+              value={SubjectTime}
+              onChange={changeSubjectTime}
+              style={{
+                width: "100%",
+                marginBottom: "12px",
+              }}
+              InputProps={{
+                style: {},
+              }}
+            />
+            과목 교수 이름
+            <TextField
+              id="outlined-textarea"
+              variant="standard"
+              value={SubjectPName}
+              onChange={changeSubjectPName}
+              style={{
+                width: "100%",
+                marginBottom: "12px",
+              }}
+              InputProps={{
+                style: {},
+              }}
+            />
           </div>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose3} style={{ color: "#072e5d" }}>
+          <Button onClick={addSubject} style={{ color: "#072e5d" }}>
             확인
           </Button>
           <Button onClick={handleClose3} style={{ color: "#072e5d" }}>
+            닫기
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog
+        open={open4}
+        onClose={handleClose4}
+        PaperProps={{ sx: { width: "60%", height: "60%" } }}
+      >
+        <DialogTitle style={{ background: "#072e5d", color: "white" }}>
+          과목 수정
+        </DialogTitle>
+        <DialogContent
+          style={{
+            marginTop: "15px",
+            display: "flex",
+            flexDirection: "column",
+            overflowY: "scroll",
+          }}
+        >
+          <div className="resume-dialog-wrapper" style={{ height: "80%" }}>
+            과목 이름
+            <TextField
+              id="outlined-textarea"
+              variant="standard"
+              value={SubjectName}
+              onChange={changeSubjectName}
+              style={{
+                width: "100%",
+                marginBottom: "12px",
+              }}
+              InputProps={{
+                style: {},
+              }}
+            />
+            과목 코드
+            <TextField
+              id="outlined-textarea"
+              variant="standard"
+              value={SubjectCode}
+              onChange={changeSubjectCode}
+              style={{
+                width: "100%",
+                marginBottom: "12px",
+              }}
+              InputProps={{
+                style: {},
+              }}
+            />
+            과목 년도
+            <TextField
+              id="outlined-textarea"
+              variant="standard"
+              value={SubjectYear}
+              onChange={changeSubjectYear}
+              style={{
+                width: "100%",
+                marginBottom: "12px",
+              }}
+              InputProps={{
+                style: {},
+              }}
+            />
+            과목 학기
+            <TextField
+              id="outlined-textarea"
+              variant="standard"
+              value={SubjectSemester}
+              onChange={changeSubjectSemester}
+              style={{
+                width: "100%",
+                marginBottom: "12px",
+              }}
+              InputProps={{
+                style: {},
+              }}
+            />
+            과목 시간
+            <TextField
+              id="outlined-textarea"
+              variant="standard"
+              value={SubjectTime}
+              onChange={changeSubjectTime}
+              style={{
+                width: "100%",
+                marginBottom: "12px",
+              }}
+              InputProps={{
+                style: {},
+              }}
+            />
+            과목 교수 이름
+            <TextField
+              id="outlined-textarea"
+              variant="standard"
+              value={SubjectPName}
+              onChange={changeSubjectPName}
+              style={{
+                width: "100%",
+                marginBottom: "12px",
+              }}
+              InputProps={{
+                style: {},
+              }}
+            />
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={subjectChange} style={{ color: "#072e5d" }}>
+            확인
+          </Button>
+          <Button onClick={handleClose4} style={{ color: "#072e5d" }}>
+            닫기
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog
+        open={open5}
+        onClose={handleClose5}
+        PaperProps={{ sx: { width: "60%", height: "60%" } }}
+      >
+        <DialogTitle style={{ background: "#072e5d", color: "white" }}>
+          팀 정보 보기
+        </DialogTitle>
+        <DialogContent
+          style={{
+            marginTop: "15px",
+            display: "flex",
+            flexDirection: "column",
+            overflowY: "scroll",
+          }}
+        >
+          <div className="resume-dialog-wrapper" style={{ height: "80%" }}>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <div>팀이름 : {teamInfo[1]}</div>
+              <div>팀 주제 : {teamInfo[2]}</div>
+              <div style={{ display: "flex" }}>
+                팀 설명 : {"  "}
+                <div dangerouslySetInnerHTML={{ __html: teamInfo[3] }} />
+              </div>
+              <div>팀 최대인원수 : {teamInfo[4]}</div>
+              <div>팀 현재인원수 : {teamInfo[5]}</div>
+            </div>
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose5} style={{ color: "#072e5d" }}>
             닫기
           </Button>
         </DialogActions>
